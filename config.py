a1fa75d1a4d1a681fa71dbb9ec8c453c6b15c7b3
Abhay-1812 <dhillonabhay1@gmail.com>
2021-06-09 12:33:32 +0530

    config file updated

--- config.py
+++ config.py
@@ -7,9 +7,10 @@ class Config(object):
     'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 10
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or "smtp.googlemail.com"
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 465)
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or "dhillonabhay2@gmail.com"
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or "Iphone6s"
     ADMINS = ['dhillonabhay2.com']
